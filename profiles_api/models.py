57c3a22852230b9ae3c38fb79551f75502df5ea0
User <user@Users-MacBook-Air.local>
2023-12-03 03:58:24 +0530

    add files

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -5,30 +5,26 @@ from  django.contrib.auth.models import BaseUserManager
 
 
 class UserProfileManager(BaseUserManager):
-    """Manager for user profiles"""
-
-    def create_user(self,email,name,password=None):
-        """Create a new user profiles"""
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
-            raise ValueError('User must have an email address')
-
+            raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
-
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-
         return user
 
-    def create_superuser(self, email, name, password):
-        """Create a new user profiles"""
-        user = self.create_user(email, name, password)
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
 
-        user.is_superuser = True
-        user.is_staff = True
-        user.save(using=self._db)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
 
-        return user
+        return self.create_user(email, password, **extra_fields)
+
 
 
 class UserProfile(AbstractBaseUser,PermissionsMixin):
